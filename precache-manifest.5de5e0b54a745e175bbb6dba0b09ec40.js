a5a629d42ea22b66d1a88419eb6b39dd1d6eb4b2
artas <artasss47@gmail.com>
2020-05-12 12:07:19 +0200

    Updates

---
+++ precache-manifest.5de5e0b54a745e175bbb6dba0b09ec40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34d5e3df40e39f6487e59b187ae5ef4",
+    "url": "/weather-forecast-widget/index.html"
+  },
+  {
+    "revision": "8437fec2ef877d91c268",
+    "url": "/weather-forecast-widget/static/css/main.74f126d3.chunk.css"
+  },
+  {
+    "revision": "1a073e4f5e862a1a5898",
+    "url": "/weather-forecast-widget/static/js/2.8eb79576.chunk.js"
+  },
+  {
+    "revision": "b9ee7cb5ade0c6c7caae405328bf0f8f",
+    "url": "/weather-forecast-widget/static/js/2.8eb79576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8437fec2ef877d91c268",
+    "url": "/weather-forecast-widget/static/js/main.a8faf080.chunk.js"
+  },
+  {
+    "revision": "0dc4bfddcf5040102178",
+    "url": "/weather-forecast-widget/static/js/runtime-main.42b0f94b.js"
+  },
+  {
+    "revision": "858a94d33c039620e126239f50d4a520",
+    "url": "/weather-forecast-widget/static/media/cloud-rain.858a94d3.svg"
+  },
+  {
+    "revision": "5670c19a5ba6d65aa32f90433fc8689c",
+    "url": "/weather-forecast-widget/static/media/cloud-sun.5670c19a.svg"
+  },
+  {
+    "revision": "6d7c11c881a6eaecf152bf15766b9670",
+    "url": "/weather-forecast-widget/static/media/cloud.6d7c11c8.svg"
+  },
+  {
+    "revision": "ac92c34cf8c6d73d883503d3fe8df90c",
+    "url": "/weather-forecast-widget/static/media/direction.ac92c34c.svg"
+  },
+  {
+    "revision": "4db24c0e662ca0fbacc25b3c959afedc",
+    "url": "/weather-forecast-widget/static/media/sun.4db24c0e.svg"
+  }
+]);
